94fb645fc1f62c5004e7465bf51d63c35e1d0b18
alameen <alameen.info@gmail.com>
2022-03-03 22:34:30 +0530

    updated rds module

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 module "vpc" {
   source   = "github.com/tecbrix/terraform-g42-vpc"
 }
+module "rds" {
+  depends_on              = [module.vpc]
+  source                  = "github.com/tecbrix/terraform-g42-rds"
+  subnet_id               = module.vpc.subnet_id
+  vpc_id                  = module.vpc.vpc_id
+}
