fc50f3007720ff7b37b3b5404ce754b216f354a0
alameen <alameen.info@gmail.com>
2022-03-07 22:11:45 +0530

    testing

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
-module "vpc" {
-  source   = "github.com/tecbrix/terraform-g42-vpc"
-}
-module "rds" {
-  depends_on              = [module.vpc]
-  source                  = "github.com/tecbrix/terraform-g42-rds"
-  subnet_id               = module.vpc.subnet_id
-  vpc_id                  = module.vpc.vpc_id
-}
+# module "vpc" {
+#   source   = "../terraform-g42-vpc"
+# }
+# module "rds" {
+#   depends_on              = [module.vpc]
+#   source                  = "../terraform-g42-rds"
+#   subnet_id               = module.vpc.subnetid
+#   vpc_id                  = module.vpc.vpcid
+#   vpc_cidr                = module.vpc.vpc_cidr
+# }
+
+# module "cce" {
+#   depends_on              = [module.vpc]
+#   source                  = "../terraform-g42-cce"
+#   subnet_id               = module.vpc.subnetid
+#   vpc_id                  = module.vpc.vpcid
+# }
