2457a8892d1395cd145fc34c4b8d0c484ae30ed5
alameen <alameen.info@gmail.com>
2022-03-08 23:06:29 +0530

    testing, enabling rds module

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ module "vpc" {
 }
 
 module "rds" {
-  depends_on       = [module.vpc]
-  source           = "github.com/tecbrix/terraform-g42-rds"
-  subnet_id        = module.vpc.subnetid
-  vpc_id           = module.vpc.vpcid
-  remote_ip_prefix = module.vpc.vpc_cidr
+  depends_on = [module.vpc]
+  source     = "github.com/tecbrix/terraform-g42-rds"
+  subnet_id  = module.vpc.subnetid
+  vpc_id     = module.vpc.vpcid
+  #vpc_cidr   = module.vpc.vpc_cidr
 }
 # module "cce" {
 #   depends_on              = [module.vpc]
