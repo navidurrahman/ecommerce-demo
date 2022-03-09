1e7ba50af0e8d454f5de2482f180a9eb11c16ad1
alameen <alameen.info@gmail.com>
2022-03-09 18:36:28 +0530

    testing, enabling cce module

--- main.tf
+++ main.tf
@@ -8,10 +8,9 @@ module "rds" {
   vpc_id     = module.vpc.vpcid
   vpc_cidr   = module.vpc.vpc_cidr
 }
-module "cce" {
-  depends_on = [module.vpc]
-  source     = "github.com/tecbrix/terraform-g42-cce"
-  subnet_id  = module.vpc.subnetid
-  vpc_id     = module.vpc.vpcid
-}
-###
+# module "cce" {
+#   depends_on = [module.vpc]
+#   source     = "github.com/tecbrix/terraform-g42-cce"
+#   subnet_id  = module.vpc.subnetid
+#   vpc_id     = module.vpc.vpcid
+# }
